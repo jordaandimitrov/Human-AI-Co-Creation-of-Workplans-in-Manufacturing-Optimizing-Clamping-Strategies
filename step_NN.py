e890680c9a7bb55585cc11286720b09efe6fe6b3
Jordaan <jordaan.dimitrov@gmail.com>
2025-10-25 11:42:00 +0200

    test

--- step_NN.py
+++ step_NN.py
@@ -102,6 +102,7 @@ def extract_face_features(shape, apply_local_frame=True):
 
     # Compute all vertices for PCA
 
+
     all_vertices = []
     for face in all_faces:
         loc = face.Location()
